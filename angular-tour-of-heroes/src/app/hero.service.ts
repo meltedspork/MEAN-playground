4acb89e63d9e42dcd0c2b6818309df543a6a1c5b
Jason Salerno <jason@meltedspork.com>
2018-05-13 18:24:08 -0500

    update get hero by id in service

--- angular-tour-of-heroes/src/app/hero.service.ts
+++ angular-tour-of-heroes/src/app/hero.service.ts
@@ -20,20 +20,25 @@ export class HeroService {
   ) { }
   private heroesUrl = 'api/heroes';  // URL to web api
 
+  /** GET heroes from the server */
   getHeroes(): Observable<Hero[]> {
     // TODO: send the message _after_ fetching the heroes
     this.messageService.add('HeroService: fetched heroes');
     return this.http.get<Hero[]>(this.heroesUrl)
     .pipe(
+      tap(heroes => this.log(`fetched heroes`)),
       catchError(this.handleError('getHeroes', []))
     );
   }
   
-
+  /** GET hero by id. Will 404 if id not found */
   getHero(id: number): Observable<Hero> {
-    // TODO: send the message _after_ fetching the hero
+    const url = `${this.heroesUrl}/${id}`;
     this.messageService.add(`HeroService: fetched hero id=${id}`);
-    return of(HEROES.find(hero => hero.id === id));
+    return this.http.get<Hero>(url).pipe(
+      tap(_ => this.log(`fetched hero id=${id}`)),
+      catchError(this.handleError<Hero>(`getHero id=${id}`))
+    );
   }
 
   /**
