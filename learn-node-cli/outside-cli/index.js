713bc0d4aff12796be746451126f8664f108d4e6
Jason Salerno <jason@meltedspork.com>
2018-05-15 15:15:28 -0500

    added minimist

--- learn-node-cli/outside-cli/index.js
+++ learn-node-cli/outside-cli/index.js
@@ -1,3 +1,9 @@
+const minimist = require('minimist')
+
 module.exports = () => {
   console.log('Welcome to the outside!')
+  console.log('----> argv', process.argv)
+
+  const args = minimist(process.argv.slice(2))
+  console.log('----> args', args)
 }
