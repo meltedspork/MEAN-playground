457939884af10d10336726bfad171310ff3f570e
Jason Salerno <jasons@jellyvision.com>
2018-05-25 13:46:32 -0500

    implement get all notes logic

--- node-easy-notes-app/app/controllers/note.controller.js
+++ node-easy-notes-app/app/controllers/note.controller.js
@@ -22,12 +22,18 @@ exports.create = (req, res) => {
     res.status(500).send({
       message: err.message || 'Some error occurred while creating the Note.'
     })
-  })
+  });
 };
 
 // Retrieve and return all notes from the database
 exports.findAll = (req, res) => {
-
+  Note.find().then(notes => {
+    res.send(notes);
+  }).catch(err => {
+    res.status(500).send({
+      message: err.message || 'Some error occurred while retrieving notes.'
+    })
+  });
 };
 
 // Find a single note with a noteId
