e84e25e701189c966d008b85641d7cfc8b51f27a
Jason Salerno <jasons@jellyvision.com>
2018-05-25 13:42:18 -0500

    implement note create logic

--- node-easy-notes-app/app/controllers/note.controller.js
+++ node-easy-notes-app/app/controllers/note.controller.js
@@ -2,7 +2,27 @@ const Note = require('../models/note.model');
 
 // Create and Save a new Note
 exports.create = (req, res) => {
+  // Validate request
+  if(!req.body.content) {
+    return res.status(400).send({
+      message: 'Note content can not be empty'
+    });
+  }
 
+  // Create a Note
+  const note = new Note({
+    title: req.body.title || 'Untitled Note',
+    content: req.body.content
+  });
+
+  // Save Note in the database
+  note.save().then(data => {
+    res.sent(data);
+  }).catch(err => {
+    res.status(500).send({
+      message: err.message || 'Some error occurred while creating the Note.'
+    })
+  })
 };
 
 // Retrieve and return all notes from the database
