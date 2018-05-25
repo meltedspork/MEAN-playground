2bed06b7d5dfd86d85b46277637c4c8dc5599f60
Jason Salerno <jasons@jellyvision.com>
2018-05-25 14:24:45 -0500

    refactoring status for note id not found error

--- node-easy-notes-app/app/controllers/note.controller.js
+++ node-easy-notes-app/app/controllers/note.controller.js
@@ -1,5 +1,11 @@
 const Note = require('../models/note.model');
 
+function statusForNoteIdNotFound(noteId) {
+  return res.status(404).send({
+    message: `Note not found with id ${noteId}`
+  });
+}
+
 // Create and Save a new Note
 exports.create = (req, res) => {
   // Validate request
@@ -40,16 +46,12 @@ exports.findAll = (req, res) => {
 exports.findOne = (req, res) => {
   Note.findById(req.params.noteId).then(note => {
     if(!note) {
-      return res.status(404).send({
-        message: `Note not found with id ${req.params.noteId}`
-      });
+      return statusForNoteIdNotFound(req.params.noteId);
     }
     res.send(note);
   }).catch(err => {
     if (err.kind === 'ObjectId') {
-      return res.status(404).send({
-        message: `Note not found with id ${req.params.noteId}`
-      })
+      return statusForNoteIdNotFound(req.params.noteId);
     }
     return res.status(500).send({
       message: `Error retrieving note with id ${req.params.noteId}`
@@ -72,16 +74,12 @@ exports.update = (req, res) => {
     content: req.body.content
   }, {new: true}).then(note => {
     if (!note) {
-      return res.status(404).send({
-        message: `Note not found with id ${req.params.noteId}`
-      });
+      return statusForNoteIdNotFound(req.params.noteId);
     }
     res.send(note);
   }).catch(err => {
     if (err.kind === 'ObjectId') {
-      return res.status(404).send({
-        message: `Note not found with id ${req.params.noteId}`
-      })
+      return statusForNoteIdNotFound(req.params.noteId);
     }
     return res.status(500).send({
       message: `Error updating note with id ${req.params.noteId}`
@@ -93,18 +91,14 @@ exports.update = (req, res) => {
 exports.delete = (req, res) => {
   Note.findByIdAndRemove(req.params.noteId).then(note => {
     if(!note) {
-      return res.status(404).send({
-        message: `Note not found with id ${req.params.noteId}`
-      })
+      return statusForNoteIdNotFound(req.params.noteId);
     }
     res.send({
       message: 'Note deleted successfully!'
     });
   }).catch(err => {
     if (err.kind === 'ObjectId' || err.name === 'NotFound') {
-      return res.status(404).send({
-        message: `Note not found with id ${req.params.noteId}`
-      });
+      return statusForNoteIdNotFound(req.params.noteId);
     }
     return res.status(500).send({
       message: `Could not delete note with id ${req.params.noteId}`
