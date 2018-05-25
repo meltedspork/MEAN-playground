6c1dfc4a7b96c3b1cadf626c8ce9d8e44cbe10c4
Jason Salerno <jasons@jellyvision.com>
2018-05-25 13:38:50 -0500

    add inital note controller

---
+++ node-easy-notes-app/app/controllers/note.controller.js
@@ -0,0 +1,25 @@
+const Note = require('../models/note.model');
+
+// Create and Save a new Note
+exports.create = (req, res) => {
+
+};
+
+// Retrieve and return all notes from the database
+exports.findAll = (req, res) => {
+
+};
+
+// Find a single note with a noteId
+exports.findOne = (req, res) => {
+
+};
+
+// Update a note identified by the noteId in the request
+exports.update = (req, res) => {
+
+};
+
+exports.delete = (req, res) => {
+
+};
