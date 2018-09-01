7423ab864cccbcaa56ea0469319a842dc0f7ba81
Jason Salerno <jason@meltedspork.com>
2018-09-01 08:57:50 -0500

    add ab command example

--- advanced-concepts/index.js
+++ advanced-concepts/index.js
@@ -1,8 +1,31 @@
-const express = require('express');
-const app = express();
+const cluster = require('cluster');
 
-app.get('/', (req, res) => {
-  res.send('Hi there');
-})
+console.log(cluster.isMaster);
 
-app.listen(3000);
+if (cluster.isMaster) {
+  cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+  // cluster.fork();
+} else {
+  const express = require('express');
+  const app = express();
+
+  function doWork(duration) {
+    const start = Date.now();
+    while(Date.now() - start < duration) {
+      // console.log(Date.now() - start);
+    }
+  }
+
+  app.get('/', (req, res) => {
+    doWork(5000);
+    res.send('Hi there');
+  })
+
+  app.get('/fast', (req, res) => {
+    res.send('This was fast!');
+  });
+
+  app.listen(3000);
+}
